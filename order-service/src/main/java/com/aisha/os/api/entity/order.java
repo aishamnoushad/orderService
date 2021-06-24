d0c674f21bd31e53e5f363e4727b236331210825
abcgroupuae <media@abcgroup.ae>
2021-06-24 16:07:21 +0400

    after adding few application properties

--- order-service/src/main/java/com/aisha/os/api/entity/order.java
+++ order-service/src/main/java/com/aisha/os/api/entity/order.java
@@ -1,6 +1,8 @@
 package com.aisha.os.api.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -15,6 +17,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class order {
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private String name;
 	private int qty;
